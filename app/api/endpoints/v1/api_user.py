53c8b805b28049d13385347cd30f7965b420fb9c
mohit <mohit@getphyllo.com>
2022-06-08 18:07:59 +0530

    delete apis support

--- app/api/endpoints/v1/api_user.py
+++ app/api/endpoints/v1/api_user.py
@@ -3,7 +3,6 @@ from typing import Optional, List
 from uuid import UUID
 
 from fastapi import APIRouter, Depends
-from pydantic import conint
 from sqlalchemy.orm import Session
 from starlette.requests import Request
 
@@ -32,3 +31,18 @@ async def get_user_by_id(id: UUID,
                          db: Session = Depends(get_db)
                          ) -> UserResponse:
     return user_service.get_user_by_id(id=id, db=db)
+
+
+@api_router.put("/{id}", response_model=UserResponse, status_code=HTTPStatus.OK)
+async def update_user(id: UUID, user_request: UserRequest, request: Request,
+                      db: Session = Depends(get_db)) -> UserResponse:
+    user: Optional[User] = user_service.update_user(id=id, user_request=user_request,
+                                                    request=request, db=db)
+    return user
+
+
+@api_router.delete("/{id}", status_code=HTTPStatus.OK)
+async def delete_user_by_id(id: UUID,
+                            db: Session = Depends(get_db)
+                            ):
+    user_service.delete_user_by_id(id=id, db=db)
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -6,7 +6,6 @@ from starlette.requests import Request
 
 from app.db.models.model import User
 from app.db.repository.user import user_repository
-from app.db.session import SessionLocal
 from app.schemas.user import UserRequest, UserResponse
 
 
@@ -15,18 +14,31 @@ def create_user(user_request: UserRequest, request: Request, db: Session):
     user.name = user_request.name
     user.age = user_request.age
     user.context = request.url.path
-    user = user_repository.create(db=db, obj_in=user)
+    user: User = user_repository.create(db=db, obj_in=user)
     return user
 
 
-def get_user_by_id(id: UUID, db: SessionLocal):
-    user = user_repository.get_by_id(db=db, id=id)
+def get_user_by_id(id: UUID, db: Session):
+    user: Optional[User] = user_repository.get_by_id(db=db, id=id)
     return user
 
 
-def get_users(db: SessionLocal) \
+def get_users(db: Session) \
         -> List[UserResponse]:
     users: Optional[List[User]] = user_repository.get(
         db=db)
 
     return users
+
+
+def delete_user_by_id(id: UUID, db: Session):
+    user_repository.delete_by_id(db=db, id=id)
+
+
+def update_user(id: UUID, user_request: UserRequest, request, db):
+    user: Optional[User] = user_repository.get_by_id(db=db, id=id)
+    user.name = user_request.name
+    user.age = user_request.age
+    user.context = request.url.path
+    user: User = user_repository.update(db=db, db_obj=user)
+    return user
