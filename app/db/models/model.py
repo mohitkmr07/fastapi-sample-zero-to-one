d10e9977d1b53f156564ff2931a613e1b636c643
mohit <mohit@getphyllo.com>
2022-06-08 15:11:46 +0530

    initial database tables commit

---
+++ app/db/models/model.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from typing import Optional, cast
+import uuid
+from sqlalchemy import Column, String, DateTime, Integer
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.dialects.postgresql import UUID
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class CommonBase:
+    __tablename__ = 'common_base'
+    # change your schema here
+    # __table_args__ = ({'schema': 'core_schema'})
+
+    id = cast(UUID(as_uuid=True),
+              Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True))  # type: ignore
+    created_by = cast(Optional[str], Column(String))
+    created_at = cast(datetime, Column(DateTime(timezone=False), default=datetime.utcnow))
+    updated_by = cast(Optional[str], Column(String))
+    updated_at = cast(datetime, Column(DateTime(timezone=False), default=datetime.utcnow, onupdate=datetime.utcnow))
+    context = cast(Optional[str], Column(String))
+
+
+class User(Base, CommonBase):
+    __tablename__ = 'user'
+
+    age = cast(int, Column(Integer))
+    name = cast(str, Column(String(100), nullable=False))
