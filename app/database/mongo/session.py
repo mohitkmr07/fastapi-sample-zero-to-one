2f511751409b4154c279d6fbcee981c4f271f23f
mohit <mohit@getphyllo.com>
2022-07-06 13:46:14 +0530

    mongo db base code

--- app/database/mongo/session.py
+++ app/database/mongo/session.py
@@ -1,9 +1,12 @@
 import motor.motor_asyncio
 
-MONGO_DETAILS = "mongodb://test:test123@localhost:27017"
+from app.core.config import settings
 
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS,
+client = motor.motor_asyncio.AsyncIOMotorClient(host=settings.MONGO_SERVER,
+                                                username=settings.MONGO_USER,
+                                                password=str(settings.MONGO_PASSWORD),
+                                                port=settings.MONGO_PORT,
                                                 maxPoolSize=10,
                                                 minPoolSize=10)
 
-database = client.test_db
+database = getattr(client, settings.MONGO_DB)
